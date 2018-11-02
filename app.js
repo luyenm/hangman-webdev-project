8ad3d6c90732b47626d09f672a1ba74a76daea22
Arlios <luyen_mai@live.com>
2018-11-02 12:48:52 -0700

    Sign in page implementation

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // const functions = require('firebase-functions');
 
 var http = require('http'),
-    fs = require('fs')
-var port = process.env.PORT || 3000
+    fs = require('fs');
+var port = process.env.PORT || 3000;
 http.createServer(function(req, res) {
-    var url = './' + (req.url == '/' ? 'hangman.html' : req.url)
+    var url = './' + (req.url == '/' ? 'hangman.html' : req.url);
     fs.readFile(url, function(err, html) {
         if (err) {
             var message404 = "There is no such page! <a href='/'>Back to home page</a>"
@@ -25,37 +25,38 @@ http.createServer(function(req, res) {
 //  response.send("Hello from Firebase!");
 // });
 
-// const express = require('express');
-// const app = express();
-// const firebase = requre('firebase');
-//
-// const config = {
-//     apiKey: "AIzaSyDfn03oIIrJiBFyuQ7WY22YJem1USHo_kE",
-//     authDomain: "hangman-ca7fe.firebaseapp.com",
-//     databaseURL: "https://hangman-ca7fe.firebaseio.com",
-//     projectId: "hangman-ca7fe",
-//     storageBucket: "hangman-ca7fe.appspot.com",
-//     messagingSenderId: "717069141803"
-// };
-//
-// firebase.initializeApp(config);
-// let authentication = firebase.auth();
+const express = require('express');
+const app = express();
+const firebase = require('firebase');
+
+const config = {
+    apiKey: "AIzaSyDfn03oIIrJiBFyuQ7WY22YJem1USHo_kE",
+    authDomain: "hangman-ca7fe.firebaseapp.com",
+    databaseURL: "https://hangman-ca7fe.firebaseio.com",
+    projectId: "hangman-ca7fe",
+    storageBucket: "hangman-ca7fe.appspot.com",
+    messagingSenderId: "717069141803"
+};
+
+firebase.initializeApp(config);
+let authentication = firebase.auth();
+
 //
 // authentication.createUserWithEmailAndPassword(email, password).catch(function(error) {
 //     let errorCode = error.code;
 //     let errorMessage = error.message;
 // });
-//
-//
-// app.post("/index.html", (request, res) => {
-//     let req = request.body;
-//     if (req.instruction === "fetch") {
-//
-//     } else {
-//         storeScore(req.userScore, req.userName);
-//     }
-// });
-//
-// function storeScore(userScore, userName) {
-//
-// }
+
+
+app.post("/index.html", (request, res) => {
+    let req = request.body;
+    if (req.instruction === "fetch") {
+
+    } else {
+        storeScore(req.userScore, req.userName);
+    }
+});
+
+function storeScore(userScore, userName) {
+
+}
